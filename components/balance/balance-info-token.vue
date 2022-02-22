129c2255d231f5d7eca8928a9b9128e948a98e72
azizmamoyan <mailter@mail.ru>
2022-02-22 15:07:29 +0300

    Круговые графикики с долями IDO, Pre-sale....Public sale...

--- components/balance/balance-info-token.vue
+++ components/balance/balance-info-token.vue
@@ -44,7 +44,45 @@
     <small class="color-gray">
       ≈ ${{ $toUsd(symbol.toUpperCase(), balance).toLocaleString("en-US", { maximumFractionDigits: 3 }) }}
     </small>
-
+    <vc-donut
+      foreground="#EAECEF"
+      :size="175"
+      :thickness="25"
+      has-legend
+      legend-placement="right"
+      :sections="[
+        { label:'Pre-sale', value: 15, color: '#a200dd' },
+        { label:'IDO', value: 3, color: '#dd008c' },
+        { label:'Кошелек компании', value: 15, color: '#0f00dd' },
+        { label:'Фонд вознаграждения', value: 5, color: '#00dd25' },
+        { label:'Public sale', value: 62, color: '#dd8500' }
+      ]"
+    >
+      <template #legend>
+        <div class="cdc-legend">
+          <div
+            v-for="(item, idx) in [
+              { label:'Pre-sale', value: 15, percent: 15, style: {backgroundColor: '#a200dd'} },
+              { label:'IDO', value: 3, percent: 3, style: {backgroundColor: '#dd008c'} },
+              { label:'Кошелек компании', value: 15, percent: 15, style: {backgroundColor: '#0f00dd'} },
+              { label:'Фонд вознаграждения', value: 5, percent: 6, style: {backgroundColor: '#00dd25'} },
+              { label:'Public sale', value: 62, percent: 62, style: {backgroundColor: '#dd8500'} }
+            ]"
+            :key="idx"
+            :title="item.percent"
+            class="cdc-legend-item"
+          >
+            <span class="cdc-legend-item-color" :style="item.styles" />
+            <span class="cdc-legend-item-label m-r-5">
+              {{ item.label.toUpperCase() }}
+            </span>
+            <span class="cdc-legend-item-value">
+              {{ item.value }} %
+            </span>
+          </div>
+        </div>
+      </template>
+    </vc-donut>
     <ui-modal
       v-if="showModal"
       @close="closeModal"
@@ -75,6 +113,7 @@ export default {
     };
   },
   computed: {
+
     tokenName () {
       return this.sale?.name ?? "";
     },
@@ -111,4 +150,80 @@ export default {
 
 <style lang="scss" scoped>
 @import "~/assets/scss/layouts/balance-info.scss";
+.user-wallets {
+ &-box {
+   @include respond-before("md") {
+     display: flex;
+     flex-wrap: wrap;
+   }
+
+   &__left {
+     margin-bottom: 30px;
+
+     @include respond-before("md") {
+       width: 35%;
+       margin-bottom: 0;
+     }
+     @include respond-before("xl") {
+       width: 45%;
+     }
+   }
+
+   &__right {
+     @include respond-before("md") {
+       width: 65%;
+     }
+     @include respond-before("xl") {
+       width: 55%;
+     }
+
+     .cdc-container {
+       justify-content: left;
+     }
+   }
+ }
+}
+
+.cdc-container {
+  @include respond-to("md") {
+    display: block;
+  }
+}
+
+.cdc-legend {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  @include respond-before("md") {
+    margin: 0 0 0 1em;
+  }
+
+  &-item {
+    display: flex;
+    align-items: center;
+    margin: 0.5em 0;
+    font-size: 15px;
+    letter-spacing: -0.02em;
+    @include fontTTNorms("medium");
+
+    &-color {
+      height: .75em;
+      width: .75em;
+      border-radius: 2px;
+      margin-right: 0.5em;
+    }
+
+    &-label {
+      margin-right: 16px;
+    }
+
+    &-value {
+      margin-left: auto;
+    }
+  }
+}
+
 </style>
+
